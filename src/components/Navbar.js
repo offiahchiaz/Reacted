a2edf9cae3b3a758525e4a1ddbfd8f40d99fc529
offiahchiaz <offiahchiaz@gmail.com>
2021-05-08 12:39:32 +0100

    Added Link and NavLink to prevent full page refresh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a href="/" className="brand-logo">Poke'Times</a>
+                <Link to="/" className="brand-logo">Poke'Times</Link>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
